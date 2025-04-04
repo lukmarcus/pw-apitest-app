ad1b1f8ed6805a40a4557e100c9d4deb5ff934e7
Marek Szumny <lukmarcus@gmail.com>
2025-04-04 15:35:21 +0200

    performing api request test added

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, request } from "@playwright/test";
 import tags from "../test-data/tags.json";
 
 test.beforeEach(async ({ page }) => {
@@ -9,6 +9,10 @@ test.beforeEach(async ({ page }) => {
   });
 
   await page.goto("https://conduit.bondaracademy.com/");
+  await page.getByText("Sign in").click();
+  await page.getByRole("textbox", { name: "Email" }).fill("lukmarcus@test.com");
+  await page.getByRole("textbox", { name: "Password" }).fill("Test1234!");
+  await page.getByRole("button").click();
 });
 
 test("api response mock", async ({ page }) => {
@@ -31,5 +35,49 @@ test("api response mock", async ({ page }) => {
   );
   await expect(page.locator("app-article-list p").first()).toHaveText(
     "This is a MOCK test description"
+  );
+});
+
+test("delete article", async ({ page, request }) => {
+  const response = await request.post(
+    "https://conduit-api.bondaracademy.com/api/users/login",
+    {
+      data: {
+        user: {
+          email: "lukmarcus@test.com",
+          password: "Test1234!",
+        },
+      },
+    }
+  );
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+
+  const articleResponse = await request.post(
+    "https://conduit-api.bondaracademy.com/api/articles/",
+    {
+      data: {
+        article: {
+          title: "Test Article",
+          description: "Test Description",
+          body: "Test Body",
+          tagList: ["test"],
+        },
+      },
+      headers: {
+        Authorization: `Token ${accessToken}`,
+      },
+    }
+  );
+
+  expect(articleResponse.status()).toEqual(201);
+
+  await page.getByText("Global Feed").click();
+  await page.getByText("Test Article").click();
+  await page.getByRole("button", { name: "Delete Article" }).first().click();
+  await page.getByText("Global Feed").click();
+
+  await expect(page.locator("app-article-list h1").first()).not.toContainText(
+    "Test Article"
   );
 });
