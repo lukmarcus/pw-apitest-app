2ebe0b470cdd6c88bcd7ff5de191081fd0873363
Marek Szumny <lukmarcus@gmail.com>
2025-04-04 14:57:52 +0200

    small test refactor

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -8,27 +8,28 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
+  await page.goto("https://conduit.bondaracademy.com/");
+});
+
+test("api response mock", async ({ page }) => {
   await page.route("*/**/api/articles*", async (route) => {
     const response = await route.fetch();
     const responseBody = await response.json();
-    responseBody.articles[0].title = "This is a test title";
-    responseBody.articles[0].description = "This is a test description";
+    responseBody.articles[0].title = "This is a MOCK test title";
+    responseBody.articles[0].description = "This is a MOCK test description";
 
     await route.fulfill({
       body: JSON.stringify(responseBody),
     });
   });
 
-  await page.goto("https://conduit.bondaracademy.com/");
+  await page.getByText("Global Feed").click();
   await page.waitForTimeout(2000);
-});
-
-test("has title", async ({ page }) => {
   await expect(page.locator(".navbar-brand")).toHaveText("conduit");
   await expect(page.locator("app-article-list h1").first()).toHaveText(
-    "This is a test title"
+    "This is a MOCK test title"
   );
   await expect(page.locator("app-article-list p").first()).toHaveText(
-    "This is a test description"
+    "This is a MOCK test description"
   );
 });
