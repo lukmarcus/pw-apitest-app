0628617601cacda173d4d8f658918fdbd5eee017
Marek Szumny <lukmarcus@gmail.com>
2025-04-04 16:22:58 +0200

    intercepting api response test added

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -81,3 +81,66 @@ test("delete article", async ({ page, request }) => {
     "Test Article"
   );
 });
+
+test("create article", async ({ page, request }) => {
+  await page.getByText("New Article").click();
+  await page
+    .getByRole("textbox", { name: "Article Title" })
+    .fill("Playwright is awesome");
+  await page
+    .getByRole("textbox", { name: "What's this article about?" })
+    .fill("About the Playwright");
+  await page
+    .getByRole("textbox", { name: "Write your article (in markdown)" })
+    .fill(
+      "Playwright is a Node.js library to automate Chromium, Firefox, and WebKit with a single API."
+    );
+  await page.getByRole("button", { name: "Publish Article" }).click();
+
+  const articleResponse = await page.waitForResponse(
+    "https://conduit-api.bondaracademy.com/api/articles/"
+  );
+  const articleResponseBody = await articleResponse.json();
+  const articleSlug = articleResponseBody.article.slug;
+
+  await expect(page.locator(".article-page h1")).toHaveText(
+    "Playwright is awesome"
+  );
+
+  await page.getByText("Home").click();
+  await page.getByText("Global Feed").click();
+
+  await expect(page.locator("app-article-list h1").first()).toHaveText(
+    "Playwright is awesome"
+  );
+
+  const response = await request.post(
+    "https://conduit-api.bondaracademy.com/api/users/login",
+    {
+      data: {
+        user: {
+          email: "lukmarcus@test.com",
+          password: "Test1234!",
+        },
+      },
+    }
+  );
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+
+  const deleteArticleResponse = await request.delete(
+    `https://conduit-api.bondaracademy.com/api/articles/${articleSlug}`,
+    {
+      headers: {
+        Authorization: `Token ${accessToken}`,
+      },
+    }
+  );
+
+  expect(deleteArticleResponse.status()).toEqual(204);
+
+  await page.getByText("Global Feed").click();
+  await expect(page.locator("app-article-list h1").first()).not.toContainText(
+    "Playwright is awesome"
+  );
+});
