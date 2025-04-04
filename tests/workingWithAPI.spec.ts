f9cf539fa7f00ae0f4a34781091d9c25a6662503
Marek Szumny <lukmarcus@gmail.com>
2025-04-04 13:40:06 +0200

    mocking api with data added

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,8 +1,16 @@
 import { test, expect } from "@playwright/test";
+import tags from "../test-data/tags.json";
+
+test.beforeEach(async ({ page }) => {
+  await page.route("*/**/api/tags", async (route) => {
+    await route.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
+  await page.goto("https://conduit.bondaracademy.com/");
+  await page.waitForTimeout(500);
+});
 
 test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.locator(".navbar-brand")).toHaveText("conduit");
 });
