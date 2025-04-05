1e6c76750837ba1a893f042c01165034e412141d
Marek Szumny <lukmarcus@gmail.com>
2025-04-05 23:54:15 +0200

    authentication state project added

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, request } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import tags from "../test-data/tags.json";
 
 test.beforeEach(async ({ page }) => {
@@ -9,10 +9,6 @@ test.beforeEach(async ({ page }) => {
   });
 
   await page.goto("https://conduit.bondaracademy.com/");
-  await page.getByText("Sign in").click();
-  await page.getByRole("textbox", { name: "Email" }).fill("lukmarcus@test.com");
-  await page.getByRole("textbox", { name: "Password" }).fill("Test1234!");
-  await page.getByRole("button").click();
 });
 
 test("api response mock", async ({ page }) => {
